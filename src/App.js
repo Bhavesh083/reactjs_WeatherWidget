2ac48ff9a6d023107385531c36b5c8308ac1542d
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-02-04 22:03:20 +0530

    final deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useState} from 'react'
+import Exted from './Exted'
+import './App.css'
+const api = {
+  key:'5afcca12834a8cc45e42a9956a76b069',
+  base:'https://api.openweathermap.org/data/2.5/'
 }
 
-export default App;
+function App() {
+
+  const [det, setDet] = useState({});
+  const [place, setPlace] = useState('');
+
+  const getPlace = (e) =>{
+    setPlace(e.target.value);
+  }
+  const getDet = () =>{
+    setPlace('');
+    fetch(`${api.base}weather?q=${place}&units=metric&appid=${api.key}`)
+     .then(ans => ans.json())
+     .then(fin => {
+      setDet(fin);
+    });
+
+  }
+
+  return (
+    <div>
+      <div className='search'> 
+            <input type='text' onChange={(e)=>getPlace(e)} placeholder='Enter Place' value={place} />
+            <button onClick={()=>getDet()}><i class="fas fa-search"></i></button>
+      </div>
+        {(typeof det.main != "undefined") ? ( 
+          <div className={((det.main.temp < 16) ? ((det.main.temp > 5) ? 'nc' : 'c'): ((det.main.temp > 30) ? 'h' : 'nh'))} >
+            <div className='details'>  
+                <h3>{det.name} , {det.sys.country} </h3>
+            </div> 
+            <div className='temp'>
+                <h2>{det.main.temp}°C</h2>
+                <Exted  al={det.weather[0].main}/>
+            </div>
+         </div>)
+        : (
+        <div className='container'>
+         <h2 className='bnbnb'>Weather Widget</h2>
+         <i class="fas fa-cloud-sun-rain hghh"></i> </div>)}
+   </div> 
+  )
+}
+
+export default App
+
